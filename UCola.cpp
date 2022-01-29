5587fd20acb98489eda5ad97562675ac4b42543d
AlanJoel <alanjoelarias660@gmail.com>
2022-01-29 14:04:19 -0400

    Este es mi practico de Colas en C++

---
+++ UCola.cpp
@@ -0,0 +1,176 @@
+//---------------------------------------------------------------------------
+
+//#pragma hdrstop
+
+#include "UCola.h"
+#include <vcl.h>
+
+typedef int Tipo;
+const Max=50;
+class Cola{
+		private:
+			Tipo V[Max+1];
+			int Frente,Atras;
+		public:
+				Cola();
+				// Produce un Self vacío
+				void Meter(Tipo dato);
+				//Pre: La cola no está llena
+				//Inserta el dato al final de la Cola
+
+				void Sacar(Tipo &dato);
+				//Pre: La cola no está vacía
+				// Obtiene en dato el primer elemento de la Cola, eliminándolo  de la misma.
+				int SacarUltimo();
+
+				int SacarUnEle(Tipo &dato);
+
+				bool Vacia();
+				// Retorna true si y solo si la Cola está vacía, y false en caso contrario.
+				bool  Llena();
+				// Retorna true si y solo si la Cola está llena, y false en caso contrario.
+				int Cant();
+				//Retorna la cantidad de elementos del Self
+				void Borrar();
+				//Pre: La cola no está vacía
+				// Borrar el primer elemento de la Cola
+				Tipo Primero();
+				// Retorna el primer elemento de la Cola
+				String toString();
+};
+
+Cola::Cola(){
+// Produce un Self vacío
+	Frente=Atras=-1;
+}
+
+void Cola::Meter(Tipo dato){
+	if (Llena()) {
+		ShowMessage("Error Cola llena");
+		exit(1);
+	}
+
+	if((Frente==-1)&&(Atras==-1)){ // 1er inserción cuando la cola está vacía
+		Frente=Atras=0;
+		V[Atras]= dato;
+	}
+	else{
+		Atras++;
+		if (Atras>Max)
+			Atras=0;
+		V[Atras]=dato;
+	}
+}
+
+void Cola::Sacar(Tipo &dato){
+//Pre: La cola no está vacía
+// Obtiene en dato el primer elemento de la Cola, eliminándolo  de la misma.
+
+	if (Vacia()) {
+		ShowMessage("Error: Cola vacía");
+		exit(1);
+	}
+
+	dato = V[Frente];
+	if(Frente==Atras)
+		Frente=Atras=-1;
+	else{
+		Frente++;
+		if(Frente>Max)
+			Frente=0;
+	}
+}
+
+bool Cola::Vacia(){
+// Retorna true si y solo si la Cola está vacía, y false en caso contrario.
+	return((Frente==-1)&&(Atras==-1));
+}
+
+bool  Cola::Llena(){
+// Retorna true si y solo si la Cola está llena, y false en caso contrario.
+   int aux = Atras;
+   aux++;
+   if (aux>Max)
+		aux=0;
+   return (aux==Frente);
+
+}
+/*bool Cola::Llena(){
+	return (Cant()==Max+1);
+} */
+
+int Cola::Cant(){
+//Retorna la cantidad de elementos del Self
+	if(Atras>=Frente)
+		return (Atras-Frente+1);
+	else
+		return(Atras+1)+(Max-Frente+1);
+}
+
+void Cola::Borrar(){
+//Pre: La cola no está vacía
+// Borrar el primer elemento de la Cola
+	if (Vacia()) {
+		ShowMessage("Error: Cola vacía");
+		exit(1);
+	}
+	if(Frente==Atras)
+		Frente=Atras=-1;
+	else{
+		Frente++;
+		if(Frente>Max)
+			Frente=0;
+	}
+}
+Tipo Cola::Primero(){
+// Retorna el primer elemento de la Cola
+	if (Vacia()) {
+		ShowMessage("Error: Cola vacía");
+		exit(1);
+	}
+
+	return V[Frente];
+}
+Tipo Cola::SacarUltimo()
+{
+	if (Vacia())
+	{
+		ShowMessage("Error: Cola vacía");
+		exit(1);
+	}
+	return V[Atras];
+}
+Tipo Cola::SacarUnEle(Tipo &dato)
+{
+	if (Frente==Atras)
+	{
+		if (Frente==dato)
+		{
+			return V[Frente];
+		}
+	}else
+	{   int aux=Frente-1;
+		if (aux<Max)
+		{
+			aux++;
+			if (dato==V[aux]) {
+                return V[aux];
+			}
+		}
+	}
+}
+String Cola::toString(){
+	String cad="";
+	int k = Frente;
+	while(k!=Atras){
+		cad = cad + IntToStr(V[k])+" , ";
+		k++;
+		if(k>Max)
+			k=0;
+	}
+	cad = cad + IntToStr(V[Atras]);
+	return cad;
+}
+
+//---------------------------------------------------------------------------
+//#pragma package(smart_init)
---
+++ UPrincipal.dfm
@@ -0,0 +1,117 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 322
+  ClientWidth = 634
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 48
+    Top = 47
+    Width = 12
+    Height = 19
+    Caption = 'Q'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -16
+    Font.Name = 'Tahoma'
+    Font.Style = [fsBold]
+    ParentFont = False
+  end
+  object Edit1: TEdit
+    Left = 80
+    Top = 40
+    Width = 513
+    Height = 21
+    TabOrder = 0
+  end
+  object Button1: TButton
+    Left = 32
+    Top = 104
+    Width = 75
+    Height = 25
+    Caption = 'Vacia'
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 113
+    Top = 104
+    Width = 75
+    Height = 25
+    Caption = 'Llena'
+    TabOrder = 2
+    OnClick = Button2Click
+  end
+  object Button3: TButton
+    Left = 194
+    Top = 104
+    Width = 75
+    Height = 25
+    Caption = 'Meter'
+    TabOrder = 3
+    OnClick = Button3Click
+  end
+  object Button4: TButton
+    Left = 275
+    Top = 104
+    Width = 75
+    Height = 25
+    Caption = 'Sacar'
+    TabOrder = 4
+    OnClick = Button4Click
+  end
+  object Button5: TButton
+    Left = 356
+    Top = 104
+    Width = 75
+    Height = 25
+    Caption = 'Cant'
+    TabOrder = 5
+    OnClick = Button5Click
+  end
+  object Button6: TButton
+    Left = 437
+    Top = 104
+    Width = 75
+    Height = 25
+    Caption = 'Borrar'
+    TabOrder = 6
+    OnClick = Button6Click
+  end
+  object Button7: TButton
+    Left = 518
+    Top = 104
+    Width = 75
+    Height = 25
+    Caption = 'Primero'
+    TabOrder = 7
+    OnClick = Button7Click
+  end
+  object Button8: TButton
+    Left = 275
+    Top = 135
+    Width = 75
+    Height = 25
+    Caption = 'Sacar Ulti'
+    TabOrder = 8
+    OnClick = Button8Click
+  end
+  object Button9: TButton
+    Left = 275
+    Top = 166
+    Width = 75
+    Height = 25
+    Caption = 'SacarNumero'
+    TabOrder = 9
+    OnClick = Button9Click
+  end
+end
